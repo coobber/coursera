372ba4c5021a22de3f6fe2058d8184a29478d42d
coobber <coobber@yandex.ru>
2018-05-01 04:26:39 +0300

    reshenie

--- week2_spechki.py
+++ week2_spechki.py
@@ -1,68 +1,34 @@
 l1 = int(input())
 r1 = int(input())
-
 l2 = int(input())
 r2 = int(input())
-
 l3 = int(input())
 r3 = int(input())
-
-d1 = r1 - l1
-d2 = r2 - l2
-d3 = r3 - l3
-
-
-delta = r1 - l2
-
-if delta <= 0:
-    t = delta * -1
-    if d3 <= t:
-        print(3)
-    elif d2 <= t:
-        print(2)
-    else:
+if 0 <= l1 < r1 <= 100 and 0 <= l2 < r2 <= 100 and 0 <= l3 < r3 <= 100:
+    if (l1 <= r2 and l2 <= r1 and l2 <= r3 and l3 <= r2)\
+            or (l2 <= r1 and l1 <= r2 and l1 <= r3 and l3 <= r1)\
+            or (l2 <= r3 and l3 <= r2 and l3 <= r1 and l1 <= r3)\
+            or (l3 <= r2 and l2 <= r3 and l2 <= r1 and l1 <= r2)\
+            or (l3 <= r1 and l1 <= r3 and l1 <= r2 and l2 <= r1)\
+            or (l1 <= r2 and l3 <= r1 and l3 <= r2 and l2 <= r3):
         print(0)
-elif delta > 0:
-    # если левый край спички2 лежит внутри спички1
-    if r1 >= l2 and r1 < r2:
-        t = r2 - l3
-        if t < 0:
+    else:
+        d1 = r1 - l1
+        d2 = r2 - l2
+        d3 = r3 - l3
+        if (l3 > r2 and d1 >= l3 - r2)\
+                or (l2 > r3 and d1 >= l2 - r3)\
+                or (l2 <= r3 and l3 <= r2):
+            print(1)
+        elif (l3 > r1 and d2 >= l3 - r1)\
+                or (l1 > r3 and d2 >= l1 - r3)\
+                or (l1 <= r3 and l3 <= r1):
+            print(2)
+        elif (l1 > r2 and d3 >= l1 - r2)\
+                or (l2 > r1 and d3 >= l2 - r1)\
+                or (l1 <= r2 and l2 <= r1):
             print(3)
         else:
-            t = r3 - l1
-            if t < 0:
-                print(3)
-            else: print(0)
-    # если правый край спички2 лежит внутри спички1
-    if l1 >= l2 and l1 <= r2:
-        t = r2 - l3
-        if t < 0:
-            print(3)
-        else:
-            t = r3 - l1
-            if t < 0:
-                print(3)
-            else:
-                print(0)
-    # если правый край спички2 слева от левого края спички1
-    if r2 - l1 < 0:
-        t = -1 * (r2 - l1)
-        if t >= d3:
-            print(3)
-        else:
-            t = r3 - l1
-            if t < 0:
-                print(3)
-            else:
-                print(0)
-
-
-
-
-
-
-
-
-
-
-
+            print(-1)
+else:
+    print(-1)
